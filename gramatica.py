cda68e57cc07797672a28eed1e98b6c3374509d2
m0iino <moino.cristian@gmail.com>
2020-06-02 22:53:06 -0600

    ventana y gramatica
    primer commit

---
+++ gramatica.py
@@ -0,0 +1,75 @@
+tokens = (
+    'MAIN',
+    'LABEL',
+    'GOTO',
+    'UNSET',
+    'PRINT',
+    'READ',
+    'EXIT',
+    'DOLAR',
+    'PAR1',
+    'PAR2',
+    'COR1',
+    'COR2',
+    'ABS',
+    'DOSPUNTOS',
+    'PTOCOMA',
+    'INT',
+    'CHAR',
+    'FLOAT',
+    'MAS',
+    'MENOS',
+    'POR',
+    'DIV',
+    'MOD',
+    'ENTERO',
+    'DECIMAL',
+    'CARACTER',
+    'CADENA',
+    'NOT',
+    'OR',
+    'AND',
+    'XOR',
+    'IGUALIGUAL',
+    'NOTIGUAL',
+    'MAYORIGUAL',
+    'MENORIGUAL',
+    'MAYOR',
+    'MENOR',
+    'ARRAY'
+)
+
+#TOKENS
+t_MAIN = r'main'
+t_LABEL = r'label'
+t_GOTO = r'goto'
+t_UNSET = r'unset'
+t_PRINT = r'print'
+t_READ = r'read'
+t_EXIT = r'exit'
+t_DOLAR = r'\$'
+t_PAR1 =r'\('
+t_PAR2 = '\)'
+t_COR1 = r'\['
+t_COR2 = r'\]'
+t_ABS = r'abs'
+t_DOSPUNTOS = r'\:'
+t_PTCOMA = r'\;'
+t_INT = r'int'
+t_CHAR = r'char'
+t_FLOAT = r'float'
+t_MAS = r'\+'
+t_MENOS = r'-'
+t_POR = r'\*'
+t_DIV = r'/'
+t_MOD = r'\%'
+t_NOT = r'\!'
+t_OR = r'\^'
+t_XOR =
+t_IGUALIGUAL =
+t_NOTIGUAL =
+t_MAYORIGUAL =
+t_MENORIGUAL =
+t_MAYOR =
+t_MENOR =
+t_ARRAY =
