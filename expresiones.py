56a41d9df45c2a95b00e8defbe6245a541ce5ab9
m0iino <moino.cristian@gmail.com>
2020-06-06 10:41:25 -0600

    comit 2
    comit 2

---
+++ expresiones.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 00:04:07 2020
+
+@author: moino
+"""
+
+from enum import Enum
+
+class OPERACION_ARITMETICA(Enum) :
+    MAS = 1
+    MENOS = 2
+    POR = 3
+    DIVIDIDO = 4
+    RESIDUO = 5
+    ABSOLUTO = 6
+    
+class OPERACION_LOGICA(Enum) :
+    MAYOR_QUE = 1
+    MENOR_QUE = 2
+    IGUAL = 3
+    DIFERENTE = 4
+    NOT = 5
+    AND = 6
+    OR = 7
+    XOR = 8
+class OPERACION_RELACIONAL(Enum):
+    MAYOR_QUE = 1
+    MENOR_QUE = 2
+    IGUAL = 3
+    DIFERENTE = 4
+    
+class ExpresionNumerica:
+    '''
+        Esta clase representa una expresión numérica
+    '''
+
+class ExpresionBinaria(ExpresionNumerica) :
+    '''
+        Esta clase representa la Expresión Aritmética Binaria.
+        Esta clase recibe los operandos y el operador
+    '''
+
+    def __init__(self, exp1, exp2, operador) :
+        self.exp1 = exp1
+        self.exp2 = exp2
+        self.operador = operador
+        
+class ExpresionAbsoluto(ExpresionNumerica):
+    def __init__(self,exp):
+        self.exp = exp
+        
+class ExpresionNegativo(ExpresionNumerica) :
+    '''
+        Esta clase representa la Expresión Aritmética Negativa.
+        Esta clase recibe la expresion
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+
+class ExpresionNumero(ExpresionNumerica) :
+    '''
+        Esta clase representa una expresión numérica entera o decimal.
+    '''
+
+    def __init__(self, val = 0) :
+        self.val = val
+
+class ExpresionDecimal(ExpresionNumerica) :
+    '''
+        Esta clase representa una expresión numérica entera o decimal.
+    '''
+
+    def __init__(self, val = 0) :
+        self.val = val
+
+
+class ExpresionIdentificador(ExpresionNumerica) :
+    '''
+        Esta clase representa un identificador.
+    '''
+
+    def __init__(self, id = "") :
+        self.id = id
+
+
+
+class ExpresionCadena :
+    '''
+        Esta clase representa una Expresión de tipo cadena.
+    '''
+ 
+class ExpresionCaracter(ExpresionCadena) :
+    def __init__(self, val) :
+        self.val = val
+
+    
+    
+class ExpresionConcatenar(ExpresionCadena) :
+    '''
+        Esta clase representa una Expresión de tipo cadena.
+        Recibe como parámetros las 2 expresiones a concatenar
+    '''
+
+    def __init__(self, exp1, exp2) :
+        self.exp1 = exp1
+        self.exp2 = exp2
+
+class ExpresionAbsoluto(ExpresionCadena):
+    def __init__(self, val) :
+        self.val = val
+    
+class ExpresionDobleComilla(ExpresionCadena) :
+    '''
+        Esta clase representa una cadena entre comillas doble.
+        Recibe como parámetro el valor del token procesado por el analizador léxico
+    '''
+
+    def __init__(self, val) :
+        self.val = val
+
+class ExpresionCaracter(ExpresionCaracter):
+    def __init__(self,val):
+        self.val = val
+class ExpresionCadenaNumerico(ExpresionCadena) :
+    '''
+        Esta clase representa una expresión numérica tratada como cadena.
+        Recibe como parámetro la expresión numérica
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+class ExpresionCadenaCaracter(ExpresionCadena) :
+    '''
+        Esta clase representa una expresión caracter tratada como cadena.
+        Recibe como parámetro la expresión numérica
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionLogica() :
+    '''
+        Esta clase representa la expresión lógica.
+        Esta clase recibe los operandos y el operador
+    '''
+
+    def __init__(self, exp1, exp2, operador) :
+        self.exp1 = exp1
+        self.exp2 = exp2
+        self.operador = operador
---
+++ ts.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 00:15:09 2020
+
+@author: moino
+"""
+
+from enum import Enum
+
+class TIPO_DATO(Enum) :
+    NUMERO = 1
+    FLOAT = 2
+    CHAR = 3
+    CADENA = 4
+    CARACTER = 5
+    ARREGLO = 6
+    STRUCT = 7
+class Simbolo() :
+    'Esta clase representa un simbolo dentro de nuestra tabla de simbolos'
+
+    def __init__(self, id, tipo, valor) :
+        self.id = id
+        self.tipo = tipo
+        self.valor = valor
+
+class TablaDeSimbolos() :
+    'Esta clase representa la tabla de simbolos'
+
+    def __init__(self, simbolos = {}) :
+        self.simbolos = simbolos
+
+    def agregar(self, simbolo) :
+        self.simbolos[simbolo.id] = simbolo
+    
+    def obtener(self, id) :
+        if not id in self.simbolos :
+            print('Error: variable ', id, ' no definida.')
+
+        return self.simbolos[id]
+
+    def actualizar(self, simbolo) :
+        if not simbolo.id in self.simbolos :
+            print('Error: variable ', simbolo.id, ' no definida.')
+        else :
+            self.simbolos[simbolo.id] = simbolo
